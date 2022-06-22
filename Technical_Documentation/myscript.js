75fa6b17883f52ac7e2fe25cd79f039fe6687956
carlg <carlg@allrightladder.com>
2022-06-22 16:15:22 -0700

    dd

--- Technical_Documentation/myscript.js
+++ Technical_Documentation/myscript.js
@@ -1,3 +1,5 @@
+let getEl=(id)=>document.getElementById(id);//was added later,first few functions wont use it until upd
+
 function handleInput(){
   const daform=document.getElementById("input-box").value
   document.getElementById("mirror-input").innerHTML=daform;
@@ -12,10 +14,56 @@ function validateForm(){
   }
 }
 
-function showBackground(){
-  
+
+function mouseEnter(id){
+  getEl(id).addEventListener("mouseenter",getEl(id).style.backgroundColor="red");
 }
 
+function mouseLeave(id){
+  getEl(id).addEventListener("mouseleave",getEl(id).style.backgroundColor="initial");
+}
+
+function showBackground(){
+  
+  const sectionA=getEl("Section_A");
+  let elementsArr=["p1","p1b","p1c","p1d","p1e","p1f"];
+
+  elementsArr.map(x=>{
+    if(sectionA.contains(getEl(x))){
+      getEl(x).addEventListener("mouseenter",getEl(x).style.color="red");
+      getEl(x).addEventListener("mouseleave",getEl(x).style.color="white");
+    }
+  })
+
+}
+
+function childrenSections(){
+  getEl("test-div").innerHTML=document.body.innerHTML;
+}
+
+/*function addToList(){
+  const items=getEl("todo-input").value;
+  if(getEl("todo-list").innerHTML===""){
+    document.createElement("ul").setAttribute("id","la-liste");
+    let listTitle=document.createTextNode("Your List");
+    let myList=getEl("la-liste");
+    myList.appendChild(listTitle);
+    getEl("todo-list").appendChild(myList);
+  }else{
+    let newItem=document.createElement("li");
+    newItem.innerHTML=items;
+    myList.appendChild(newItem);
+  }
+  return false;
+}*/
 
 
-showBackground();
+function test(){
+  const inputVal=getEl("todo-input").value;
+  const textVal=document.createTextNode(inputVal);
+  const listInput=document.createElement("li");
+  const listItem=listInput.append(textVal);
+  getEl("todo-list").append(listItem)
+  getEl("todo-input").value="";  
+}
+//remember to create to to list with create elem,appenchild...
